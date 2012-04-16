c906ebbe5ec63801a347d4051510dea1faac9fde
crojas <a@b>
2012-04-16 22:06:22 +0200

    Step Picker Cleanup Round 2
    [git-p4: depot-paths = "//depot/nimbus/integrations/EC-Coverity/main/": change = 59416]

--- project/ec_setup.pl
+++ project/ec_setup.pl
@@ -4,7 +4,8 @@ my %runCoverity = (
     description => "Execute static analisis to C/C++, Java and C# source",
     category    => "Code Analysis"
 );
-  
+
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/Coverity - Code Analysis");  
 $batch->deleteProperty("/server/ec_customEditors/pickerStep/coverity");
 
 @::createStepPickerSteps = (\%runCoverity);
